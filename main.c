b607843c9ece9e0d6a9d27079ac7ab4f56568daf
mohamedessamx <me8624708@gmail.com>
2023-10-19 08:57:26 -0700

    l

---
+++ main.c
@@ -0,0 +1,84 @@
+#include "monty.h"
+
+stack_t *head = NULL;
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_fi(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *nod;
+
+	nod = malloc(sizeof(stack_t));
+	if (nod == NULL)
+		err(4);
+	nod->next = NULL;
+	nod->prev = NULL;
+	nod->n = n;
+	return (nod);
+}
+
+/**
+ * freenodes - Frees nodes in the stack.
+ */
+void freenodes(void)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+
+/**
+ * addto_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void addto_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *new_node;
+	(*new_node)->prev = temp;
+
+}
