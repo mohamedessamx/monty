9971e3ff4fe020d35fbf65436bb5975bb0377f14
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-19 08:27:48 -0700

    ‘hi’

---
+++ stack_function2.c
@@ -0,0 +1,96 @@
+#include "monty.h"
+
+/**
+ * _nop - Does nothing.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+/**
+ * swap_node - Swaps the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void swap_node(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "swap");
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	temp->prev = NULL;
+	*stack = temp;
+}
+
+/**
+ * add_node - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void add_node(stack_t **stack, unsigned int line_number)
+{
+	int s;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "add");
+
+	(*stack) = (*stack)->next;
+	s = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = s;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+
+/**
+ * sub_node - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void sub_node(stack_t **stack, unsigned int line_number)
+{
+	int s;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "sub");
+
+
+	(*stack) = (*stack)->next;
+	s = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = s;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+
+/**
+ * div_node - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void div_node(stack_t **stack, unsigned int line_number)
+{
+	int s;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "div");
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	s = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = s;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
